9a412505ffd90f11c035796e56562de074050f7c
Christian <c.moeller@ftapi.com>
2020-12-14 13:25:11 +0100

    verbesserungen allgemein

--- Classes/def_classes.py
+++ Classes/def_classes.py
@@ -31,115 +31,73 @@ def wurf(unter=0, ober=100):
 #TODO: Sehr viel von deinem Code hier ist nach dem Schema "Eingabe einholen, Reaktion printen"
 # Kannst du eine Methode schreiben, die für dich dieses Schema abarbeitet? Also eine Funktion, die
 # einen Fragestring, und eine Liste an möglichen antworten nimmt, und den Index der vom Spieler gegebenen Antwort zurückgibt?
-#
 
-# wenn die hp meines monsters bei 0 oder kleiner ist besteht die chance eine waffe zu finden die mein dmg erhöt oder ein potion zu finden der meine hp erhöt
-def w_oder_p(monster, gamestate):
-    if monster.HP <= 0:
-        gefundenes_item = welches_item_findest_du()
-        print(gefundenes_item.Name)
-        if type(gefundenes_item)== Classes.variablen.Wappon:
-            if gamestate.playerinventar["mainhand"] == 0:
-                gamestate.playerinventar["mainhand"] = gefundenes_item.Dmg
-                print("Du hast ein", gefundenes_item.Name, "gefunden.")
-                print("Dein Schaden erhöt sich um", gefundenes_item.Dmg)
-            elif gamestate.playerinventar["offhand"] == 0:
-                gamestate.playerinventar["offhand"] = gefundenes_item.Dmg
-                print("Du hast eine Waffe gefunden und sie in die offhand genommen")
-                print("dein schaden erhöt sich um", gefundenes_item.Dmg)
-            else:
-                print("Du findest ein", gefundenes_item.Name)
-                abfrage = input("Du hast bereits 2 waffen. Willst du die neue waffe ersetzen oder nicht?\n")
-                if abfrage == "ja":
-                    abfrage = input("welche waffe willst du ersetzen? mainhand oder offhand?\n")
-                    if abfrage == "mainhand":
-                        gamestate.playerinventar["mainhand"] = gefundenes_item.Dmg
-                        print("Deine Mainhand wurde ersetzt")
-                    else:
-                        gamestate.playerinventar["offhand"] = gefundenes_item.Dmg
-                        print("Deine offhand wurde ersetzt")
-                #TODO: Wenn ich "Ja" eintippe, wird die Waffe weggeworfen :( Frage explizit möglichkeiten ab ("ja,"nein") und gib "unbekannter Befehl" aus, wenn etwas anderes eingegeben wird
-                else:
-                    print("Okay, du hast die neue Waffe weggeworfen. Du wirst diese nie! wieder! sehen. Es ist vorbei! Aus!")
-                    return gamestate
-        elif type(gefundenes_item) == Classes.variablen.Potion:
-            Hp_pot(gamestate, gefundenes_item)
-            print("Deine hp erhöt sich um", gefundenes_item.Wert)
-        elif type(gefundenes_item) == Classes.variablen.Armor:
-            if gefundenes_item.Slot == "Helm":
-                gamestate.playerrüstung["Helm"] = gefundenes_item.Wert
-                print("Dein Helmrüstungswert hat sich erhöt")
-            elif gefundenes_item.Slot == "Brust":
-                gamestate.playerrüstung["Brust"] = gefundenes_item.Wert
-                print("Dein Brustrüstungswert hat sich erhöt")
-            elif gefundenes_item.Slot == "Beine":
-                gamestate.playerrüstung["Beine"] = gefundenes_item.Wert
-                print("Deine Beinrüstungswert hat sich erhöt")
-    return gamestate
+
 # Kampfsystem
 def Hp_pot(gamestate, potion):
     gamestate.playerhp = gamestate.playerhp + potion.Wert
     return gamestate
+
 def welches_item_findest_du():#entweder waffe, pot, oderrüstung wird erstellt und zurückgegeben
     randomizeliste = [Classes.variablen.Waffenliste, Classes.variablen.Potionliste, Classes.variablen.Rüstungsliste]
     ausgewählteliste = random.choice(randomizeliste)
     object = random.choice(ausgewählteliste)
     return object# < ein object
 
-#TODO: hier drin hast du auch 2 zentrale Ereignisse: spieler wählt kämpfen, oder spieler wählt laufen.
-# Die können auch in eine eigene Methode rein
-#def kampf (gamestate, infight):
-#    while infight: #kampf gegen ein Mosnter aus monsterlsite
-#        print("Du musst gegen ein", monster.Gattung, "kämpfen. Seine Hp betragen", monster.HP, "und er macht", monster.Dmg, "Schaden.", "Deine Hp betragen zu beginn", gamestate.playerhp)
-#        while monster.HP>0 and gamestate.playerhp>0 and infight==True: # wärend beide hp über 0 liegen wird gekämpft
-#            frage = input("Kämpfen oder Laufen?\n")
-#            if frage in ["kämpfen", "k"]:
-#                treffer = wurf(0,100)
-#                if treffer >= 30:
-#                    monster.HP = monster.HP-playerschaden
-#                    print(monster.Gattung, "hat noch ", monster.HP, "Hp")
-#                else:
-#                    gamestate.playerhp = gamestate.playerhp-Classes.monster.monsterdmg(gamestate, monster.Dmg)
-#                    print("Du hast noch", gamestate.playerhp, "Hp")
-#            elif frage == "laufen": #sollte er fliehen bestehtn eine chance von 50% das er entkommt und keinen dmg bekommt  oder einmal schaden bekommt
-#                if wurf(0,2) == 1:
-#                    print("Glück gehabt. Du entkommst ohne schaden zu bekommen\n")
-#                    infight = False
-#                else:
-#                    gamestate.playerhp = gamestate.playerhp-monster.Dmg
-#                    print("Du konntest fliegen aber", monster.Gattung, "macht dir Schaden in höhe von", monster.Dmg)
-#                    infight = False
-#            gamestate = w_oder_p(monster, gamestate)
-#        infight = False
-#    return gamestate
-
-def fight(gamestate,monster, infight):
-    playerschaden = schadenbrechenen(gamestate)
-    while monster.HP > 0 and gamestate.playerhp > 0 and infight == True:
-        if wurf(0,100)>80: #< liegt die zahl zwischen 80-100 trifft der gegner mich
+def fight(gamestate):
+    monster = Classes.monster.monsterwahl()
+    while monster.HP > 0 and gamestate.playerhp > 0:
+        if wurf(0,100)>50: #< liegt die zahl zwischen 80-100 trifft der gegner mich
             gamestate.playerhp = gamestate.playerhp-Classes.monster.monsterdmg_berechnung(gamestate, monster.Dmg)
-        if monster.HP <=0:
-            loot()
-            print("Monster tot")
-            infight = False
+            print("player bekommt schaden")
         else:
             monster.HP = monster.HP-schadenbrechenen(gamestate)
-
+            print("monster bekommt schaden")
+    if monster.HP <= 0:
+        print("Monster tot")
+        loot(gamestate)
     return gamestate
+def laufen(gs):
+    print("Du rennst wie eine kleine Muschi davon. Angsthase")
 
 
-def laufen():
-    print("Du bist davon gelaufen! Wie eine kleine...")
+def loot(gs):#<bekommt man loot?
+    if wurf(0,100)>=0:
+        print("du bekommt loot")
+        gefundenes_item = welches_item_findest_du() # das ist ein objekt
+        if type(gefundenes_item) == Classes.variablen.Wappon:
+            print("waffe")
+            loot_waffe(gs, gefundenes_item)
+        elif type(gefundenes_item) == Classes.variablen.Armor:
+            print("Rüstung")
+            loot_rüstung(gs, gefundenes_item)
+        elif type(gefundenes_item) == Classes.variablen.Potion:
+            print("potion")
+            loot_potion(gs, gefundenes_item)
+    return gs
 
-def kampf_laufen():
-    return input("Willst du \"kämpfen\" oder \"laufen\"")
-def loot():#<bekommt man loot?
-    if wurf(0,100)>0:
-        gefundenes_item = welches_item_findest_du()
-        if gefundenes_item == Classes.variablen.Wappon:
-            print("Waffe gefunden")
-        elif gefundenes_item == Classes.variablen.Armor:
-            print("rüstung gefunen")
-        elif gefundenes_item == Classes.variablen.Potion:
-            print("potion gefunden")
+def loot_waffe(gs, object):
+    if gs.playerinventar["mainhand"] == 0:
+        gs.playerinventar["mainhand"] = object.Dmg
+    elif gs.playerinventar["offhand"] == 0:
+        gs.playerinventar["offhand"] = object.Dmg
+    else:
+        print("waffe weggeworfen")
+    return gs
 
+
+def loot_rüstung(gs, object):
+    if object.Slot == "Helm":
+        gs.playerrüstung["Helm"] = object.Wert
+        print("Dein Helmrüstungswert hat sich erhöt")
+    elif object.Slot == "Brust":
+        gs.playerrüstung["Brust"] = object.Wert
+        print("Dein Brustrüstungswert hat sich erhöt")
+    elif object.Slot == "Beine":
+        gs.playerrüstung["Beine"] = object.Wert
+        print("Deine Beinrüstungswert hat sich erhöt")
+    return gs
+def loot_potion(gs, object):
+    gs.playerhp = gs.playerhp+object.Wert
+    Hp_pot(gs, object)
+    return gs
--- Classes/main.py
+++ Classes/main.py
@@ -1,11 +1,9 @@
 from Classes.def_classes import *
 from Classes.monster import *
 def begegnung (gs):
-    if wurf() >=50:
-        if kampf_laufen() == "kämpfen":
-            fight(gs, monsterwahl(), True)
-        else:
-            laufen()
+    antwortmöglichkeuten = {"kämpfen":fight, "laufen":laufen}
+    frage = "willst du kämpfen oder laufen"
+    interaktion(antwortmöglichkeuten, gs, frage)
 
 def _help(gs):
     print("Willst Du deine Hp wissen? gebe \"hp\" ein\nWillst Du deinen Dmg wissen? Gebe \"dmg\" ein\nWillst Du eine Rüstung sehen? gebe \"rüstung\" ein\n")
@@ -28,8 +26,8 @@ def rüstung_abfrage(gs):
     print("Dein Aktueller Rüstungswert ist:", gs.playerrüstung["Helm"]+gs.playerrüstung["Brust"]+gs.playerrüstung["Beine"])
 
 
-def interaktion (Antworten,gs):
-    userinput = input("Gehst du nach \"rechts\", \"links\" nach \"vorne\" oder \"zurück\"? Für mehr Optionen \"help\"\n")
+def interaktion (Antworten,gs, diefrage):
+    userinput = input(diefrage)
     if userinput in Antworten:
         Antworten[userinput](gs)
     else:
@@ -40,16 +38,14 @@ def gameloop():
     gs.playername    = input("Erst einmal. Wie heißt du?\n")
     print("Hallo", gs.playername + "."+"\nWas wirst du tun?")
     Lebendig = True
+    frage = "Welche Richtung gehst du?\n >>help<< für mehr Optionen\n"
+    Antwortmöglichkeiten = {"rechts": rechts, "links": links, "vor": vor, "zurück": zurück, "hp": hp_abfrage, "dmg": dmg_abfrage, "rüstung": rüstung_abfrage, "help": _help}
     while Lebendig == True:
         if gs.playerhp <= 0:
             Lebendig = False
             print("Game Over")
             exit()
-
-
-        Antwortmöglichkeiten = {"rechts": rechts, "links":links, "vor":vor, "zurück":zurück,"hp":hp_abfrage, "dmg":dmg_abfrage,"rüstung":rüstung_abfrage, "help":_help}
-        interaktion(Antwortmöglichkeiten, gs)
-
+        interaktion(Antwortmöglichkeiten, gs, frage)
 print("Willkommen in der Welt von \"Bitte hier Name einfügen\". Es liegt an dir.")
 
 gameloop()
