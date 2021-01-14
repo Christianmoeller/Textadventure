d3db01a5a6b49552ddb6869bd8eaea4af2678cb8
Christian <c.moeller@ftapi.com>
2021-01-14 18:12:53 +0100

    again what learnd2

--- Adventskalender/3.12.2020/Experimente.py
+++ Adventskalender/3.12.2020/Experimente.py
@@ -1,5 +1,25 @@
-f = open("3.12.2020source.txt")
+with open("3.12.2020source.txt") as map:
+    map = map.read().split("\n")
+print(map)
+down = 1
+right = 3
+x = 0
+y = 0
+count = 0
 
-liste = [line for line in f]
+for x in
 
-print(liste)
+
+
+
+
+
+#for line in map:
+#    y = y + right
+#    if y >= len(line)-1:
+#        y = y-len(line)
+#    if line[y] == "#":
+#        count +=1
+
+print(count)
+
--- Adventskalender/7.12.2020/7.12.2020.py
+++ Adventskalender/7.12.2020/7.12.2020.py
@@ -6,22 +6,18 @@ def parts_to_colors(color_part):
         color = colorstringparts[1] + " "+ colorstringparts[2]
         listofcolor.append(color)
     return listofcolor
-
 def tuple_rule(maincolor, secondcolors):
     listoftuple = []
     for color in secondcolors:
         tuple = (maincolor, color)
         listoftuple.append(tuple)
     return listoftuple
-
 def get_cointaining (rule_liste, find_color):
     colorliste  = []
     for color in rule_liste:
         if color[1] == find_color:
             colorliste.append(color[0])
     return colorliste
-
-
 def main():
 
     with open("7.12.2020 input") as f:
@@ -33,8 +29,18 @@ def main():
 
         colors = parts_to_colors(parts[1])
         rules = rules + (tuple_rule(maincolor, colors))
-    print(rules)
-    get_cointaining(rules, "shiny gold")
-
-
+    #print(rules)
+    last_level = ["shiny gold"]
+    neue_liste = []
+    done = False
+    maxliste = []
+    while not done:
+        new_level = []
+        for listelement in last_level:
+            new_level = new_level + (get_cointaining(rules, listelement))
+        maxliste = maxliste + new_level
+        print(len(set(maxliste)))
+        last_level = new_level
+        if new_level == []:
+            done = True
 main()
