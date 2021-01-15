55bbcd4c01b03aa3c5ccac2ef16bc2fb1a03bf89
moller <der_moeller@gmx.de>
2021-01-15 16:10:45 +0100

    Neuer Textadventure mit pokemon

---
+++ Textadventures/Main.py
@@ -0,0 +1,10 @@
+from Textadventures.Pokemon import *
+def main():
+    print("Wilkommen in der unglaubelichen Welt von Pokemon 2.0!\nSicher fragst du dich was wie das sein kann.\nDoch das lernst du im laufe deses Spiels.")
+    print("Zu Beginn brauchst du natürlich erstmal ein Start-Pokemon.\nBitte wähle eines der folgenen aus:\n")
+    firstpokemon = input("Schiggy-WASSER; Glumanda-FEUER, Bisasam-PFLANZE\n")
+    startpokemon(firstpokemon)
+    while True:
+        pass
+
+main()
